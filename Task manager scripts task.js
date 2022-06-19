2c6f9ab53f27f40d3f412e371323a69a8dfa9e25
Manny Ramos <96265592+ramosman1@users.noreply.github.com>
2022-06-19 01:45:54 -0700

    Add files via upload

---
+++ Task manager scripts task.js
@@ -0,0 +1,16 @@
+///// this can be use anywhere
+// this is what is important and in OBJ
+// the names here are what matter
+class Task {
+  constructor(id, title, isImportance, duration, deadline, location, status) {
+    this.id = id;
+    this.title = title;
+    this.isImportant = isImportance;
+    this.duration = duration;
+    this.deadline = deadline;
+    this.location = location;
+    this.status = status;
+
+    this.name = "Man";
+  }
+}
